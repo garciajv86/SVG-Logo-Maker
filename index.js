4f77bcc8688b873d79c55d77c1f20869eb81f4fb
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-05-19 17:14:05 -0500

    finish setting up most my folders and files along with starter code

---
+++ index.js
@@ -0,0 +1,9 @@
+// TODO: Include packages needed for this application
+const askQuestions = require("./lib/ask-questions");
+
+// TODO: Create a function to initialize app
+function init() {
+    askQuestions();
+  }
+  
+  init();
